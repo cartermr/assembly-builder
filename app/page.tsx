be286a9c24427ebaa7a07f56ddbf53311d9e1cd6
Matt Carter <mcarter@agentstitle.com>
2023-05-19 21:34:11 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,81 +1,113 @@
 'use client'
-import { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from 'react'
+import {ChangeEvent, KeyboardEvent, useEffect, useRef, useState} from 'react'
 
+const currentPart = 'FPE-351-1317'
 export default function Home() {
-  const [partNumber, setPartNumber] = useState<string>('')
-  const [serialNumber, setSerialNumber] = useState<string>('')
-  const [serialList, setSerialList] = useState<Array<string>>([])
-  const [unitCount, setUnitCount] = useState<number>(0)
+    const [partNumber, setPartNumber] = useState<string>('')
+    const [serialNumber, setSerialNumber] = useState<string>('')
+    const [serialList, setSerialList] = useState<Array<string>>([])
+    const [unitCount, setUnitCount] = useState<number>(0)
+    const [remaining, setRemaining] = useState<number>(5)
 
-  const serialInputRef = useRef<HTMLInputElement>(null)
-  const partInputRef = useRef<HTMLInputElement>(null)
+    const serialInputRef = useRef<HTMLInputElement>(null)
+    const partInputRef = useRef<HTMLInputElement>(null)
 
-  const handlePartInput = (event: ChangeEvent<HTMLInputElement>) => setPartNumber(event.currentTarget.value)
-  const handlePartKey = (event: KeyboardEvent) => {
-    if (event.key === 'Enter' && serialInputRef.current) {
-      serialInputRef.current.focus()
-      serialInputRef.current.select()
+    const handlePartInput = (event: ChangeEvent<HTMLInputElement>) => setPartNumber(event.currentTarget.value)
+    const handlePartKey = (event: KeyboardEvent) => {
+        if (event.key === 'Enter') {
+            serialInputRef.current?.focus()
+            serialInputRef.current?.select()
+        }
     }
-  }
 
-  const handleSerialInput = (event: ChangeEvent<HTMLInputElement>) => setSerialNumber(event.currentTarget.value)
-
-  const addUnit = () => {
-    console.log(partNumber)
-    console.log(serialNumber)
-    if (partNumber && serialNumber) {
-      let newList: Array<string> = [...serialList]
-      newList.push(serialNumber)
-      setUnitCount(unitCount + 1)
-      setSerialList(newList)
-      setPartNumber('')
-      setSerialNumber('')
-      partInputRef.current?.focus()
-      partInputRef.current?.select()
+    const handleSerialInput = (event: ChangeEvent<HTMLInputElement>) => setSerialNumber(event.currentTarget.value)
+    const handleSerialKey = (event: KeyboardEvent) => {
+        if (event.key === 'Enter') {
+            addUnit()
+        }
     }
-  }
 
-  return (
-    <main className="grid grid-rows-6 grid-cols-2 h-screen w-screen gap-2 p-12">
-      <section className='row-span-2 col-span-1 rounded border-solid border-2 hover:border-black flex flex-col p-3'>
-          <p className='text-lg font-semibold mb-3'>Product</p>
-          <div className='flex space-x-2'>
-            <p>Name:</p>
-            <p>FPE-351-1317</p>
-          </div>
-          <div className='flex space-x-2'>
-            <p>BOM:</p>
-            <p>FPE-351-1317-BOM</p>
-          </div>
-          <div className='flex space-x-2'>
-            <p>Description:</p>
-            <p>Cheetah Turbo</p>
-          </div>
-        </section>
-        <section className=' row-span-6 col-span-1 rounded border-solid border-2 hover:border-black flex p-3'>
-          <div className='flex flex-col w-1/2'>
-            <p className='text-lg font-semibold mb-3'>Unit Count</p>
-            <div className='h-full w-1/2 justify-items-center items-center'>
-              <p className='text-center text-3xl font-semibold'>{unitCount}</p>
+    const addUnit = () => {
+        if (partNumber && serialNumber && partNumber === currentPart) {
+            let newList: Array<string> = [...serialList]
+            newList.push(serialNumber)
+            setUnitCount(unitCount + 1)
+            setRemaining(remaining - 1)
+            setSerialList(newList)
+            setPartNumber('')
+            setSerialNumber('')
+            partInputRef.current?.focus()
+            partInputRef.current?.select()
+        }
+    }
+
+    return (
+        <main className="flex flex-col md:flex-row min-h-screen min-w-screen p-2 md:p-5 gap-2 border-2 border-red-500">
+            <div className='md:w-4/6 flex flex-col gap-4 md:gap-14 rounded border-2 hover:border-black'>
+                <section className='flex flex-col md:flex-row p-3'>
+                    <div className='flex flex-col w-full'>
+                        <p className='text-md font-semibold mb-3'>Current Build</p>
+                        <div className='flex w-full'>
+                            <div className='flex flex-col w-1/2'>
+                                <div className='text-sm flex space-x-2'>
+                                    <p>Name:</p>
+                                    <p>{currentPart}</p>
+                                </div>
+                                <div className='text-sm flex space-x-2'>
+                                    <p>Description:</p>
+                                    <p>Cheetah Turbo</p>
+                                </div>
+                                <div className='text-sm flex space-x-2'>
+                                    <p>Work Order:</p>
+                                    <p>WO123456</p>
+                                </div>
+                                <div className='text-sm flex space-x-2'>
+                                    <p>BOM:</p>
+                                    <p>FPE-351-1317-BOM</p>
+                                </div>
+                            </div>
+                            <div className='flex flex-col w-1/2'>
+                                <div className='text-sm flex space-x-2'>
+                                    <p>Qty Ordered:</p>
+                                    <p>10</p>
+                                </div>
+                                <div className='text-sm flex space-x-2'>
+                                    <p>Qty Built:</p>
+                                    <p>5</p>
+                                </div>
+                                <div className='text-sm flex space-x-2'>
+                                    <p>Qty Remaining:</p>
+                                    <p>{remaining}</p>
+                                </div>
+                                <div className='text-sm flex space-x-2'>
+                                    <p>Current Qty To Build:</p>
+                                    <p>{unitCount}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <div className='flex flex-col items-center py-3'>
+                    <section className='flex flex-col gap-10 w-10/12 lg:w-2/4'>
+                        <div className='flex flex-col'>
+                            <label>Part Number</label>
+                            <input autoFocus className='border-2' type='text' ref={partInputRef} onChange={handlePartInput} onKeyDown={handlePartKey} value={partNumber}/>
+                            <label className='mt-2'>Serial Number</label>
+                            <input className='border-2' type='text' ref={serialInputRef} onChange={handleSerialInput} onKeyDown={handleSerialKey} value={serialNumber}/>
+                        </div>
+                        <button className='rounded bg-sky-500 p-10 font-semibold' onClick={addUnit}>Add Unit</button>
+                        <button className='rounded bg-sky-500 mb-auto p-10 font-semibold'>Build Units</button>
+                    </section>
+                </div>
             </div>
-          </div>
-          <div className='flex flex-col w-1/2'>
-            <p className='text-lg font-semibold mb-3'>Serial Numbers</p>
-            {serialList.map((serial, index) => {
-              return <p key={index}>{serial}</p>
-            })}
-          </div>
-        </section>
-      <div className='row-span-4 col-span-1 rounded border-solid border-2 hover:border-black flex flex-col items-center p-10'>
-        <section className='flex flex-col p-5'>
-          <label>Part Number</label>
-          <input autoFocus className='border-2' type='text' ref={partInputRef} onChange={handlePartInput} onKeyDown={handlePartKey} value={partNumber} />
-          <label className='mt-10'>Serial Number</label>
-          <input className='border-2' type='text' ref={serialInputRef} onChange={handleSerialInput} value={serialNumber} />
-          <button className='rounded bg-sky-500 mt-10 p-3 font-semibold' onClick={addUnit}>Add Unit</button>
-          <button className='rounded bg-sky-500 mt-10 p-3 font-semibold'>Build Units</button>
-        </section>
-      </div>
-    </main>
-  )
+            <section className='md:w-2/6 flex flex-col rounded border-solid border-2 hover:border-black p-3'>
+                <p className='text-center font-semibold mb-3'>Serial Numbers Entered</p>
+                <ul className='list-disc list-inside ml-6'>
+                    {serialList.map((serial, index) => {
+                        return <li key={index}>{serial}</li>
+                    })}
+                </ul>
+            </section>
+        </main>
+    )
 }
